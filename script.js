ce7499a6b50e119a9b399a7e15f586e1fad5f752
Ben <berkhart@rocketmail.com>
2022-07-30 17:21:05 -0500

    Added script.js and modified style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement('h2');
+h2.textContent = 'This content was added by JavaScript';
+document.querySelector('body').appendChild(h2);
+
+const h3 = document.createElement('h3')
+h3.textContent = 'This was created by JavaScript as well.';
+document.querySelector('body').appendChild(h3);
